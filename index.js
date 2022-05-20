00f902c024998463b35991aa5cada2ac25e93935
James Macapagal <j.macapagal@gmail.com>
2022-05-20 13:48:45 -0500

    commit tree b7b8c2fbbe6e338b245a2fe3228e456d97c16d5a

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+
+main.remove();
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+newHeader.textContent = "YOUR-NAME is the champion!"
+
+document.body.append(newHeader)
+
 // Write your code here!
+
+
+
+const ul = document.createElement("ul")
+document.body.append(ol)
+
+for (let i = 0; i < 101; i++){
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString()
+    ul.append(li)
+}
